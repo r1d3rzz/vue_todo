6a5c25c342bdb1a6592ca9ddda7a113a90eb2994
r1d3rzz <myintthwaykhine127@gmail.com>
2022-09-21 22:36:41 +0630

    fetch json-server api and create route

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,26 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div v-for="todo in todos" :key="todo.id">
+    <router-link :to="{ name: 'todoDetail', params: { id: todo.id } }">
+      <h3>{{ todo.body }}</h3>
+    </router-link>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      todos: [],
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/todos")
+      .then((res) => res.json())
+      .then((data) => (this.todos = data))
+      .catch((err) => console.log(err.message));
+  },
+};
 </script>
+
+<style>
+</style>
---
+++ src/views/TodoDetail.vue
@@ -0,0 +1,29 @@
+<template>
+  <h3>{{ todo.detail }}</h3>
+  <button @click="homeBtn">Home</button>
+</template>
+
+<script>
+export default {
+  props: ["id"],
+  data() {
+    return {
+      todo: [],
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/todos/" + this.id)
+      .then((res) => res.json())
+      .then((data) => (this.todo = data))
+      .catch((err) => console.log(err.message));
+  },
+  methods: {
+    homeBtn() {
+      this.$router.push({ name: "home" });
+    },
+  },
+};
+</script>
+
+<style>
+</style>
